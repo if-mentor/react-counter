d799ef707e6f75cdf970cfdee6a0f5abb4f57385
tetsuro takamura <tetsuro.takamura@gmail.com>
2023-05-25 15:49:27 +0900

    add:完成版を追加

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <>Learn React</>;
-}
+import React, { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      Count: {count}
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  );
+};
 
 export default App;
